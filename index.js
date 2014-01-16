7f9b49fdd312c72cf7559b189ce1bb25e1c5429f
Wilker Lucio <wilkerlucio@gmail.com>
2014-01-16 16:49:03 -0300

    improve package.json and expose the API

---
+++ index.js
@@ -0,0 +1,6 @@
+require("coffee-script");
+
+module.exports = {
+  SearchDownload: require("./src/lib/interactors/search_download.coffee"),
+  VideoScan: require("./src/lib/interactors/video_scan.coffee")
+};
