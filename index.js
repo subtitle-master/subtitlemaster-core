43803eafe72029615ca5bc2adb4d04ce515aee0f
Wilker Lucio <wilkerlucio@gmail.com>
2014-02-24 00:04:55 -0300

    added OrganizeMedia to index

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ require("coffee-script");
 module.exports = {
   SearchDownload:    require("./lib/interactors/search_download.coffee"),
   VideoScan:         require("./lib/interactors/video_scan.coffee"),
-  AlternativeSearch: require("./lib/interactors/alternative_search.coffee")
+  AlternativeSearch: require("./lib/interactors/alternative_search.coffee"),
+  OrganizeMedia:     require("./lib/interactors/organize_media.coffee")
 };
